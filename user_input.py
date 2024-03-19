ffcfc22c37dca13cd4db2bab623dabdbf9f35b10
David Emmanuel <david3emmanuel@gmail.com>
2024-03-19 04:49:31 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Where do you live? ")
+
+print("Hello " + name + ", you are", age, "years old and live in " + location + ".")
